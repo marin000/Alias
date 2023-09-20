dd7dbc087b7896ba58b1d0d66f4950c3da86122d
Marin <mburic_ag@medable.com>
2023-09-21 00:29:38 +0200

    remove logs

--- server/service/apiSecurity.js
+++ server/service/apiSecurity.js
@@ -2,11 +2,9 @@ require('dotenv')
   .config()
 
 const allowedEmails = process.env.ALLOWED_EMAILS.split(',')
-console.log(allowedEmails)
 const checkInvitation = () => {
   return (req, res, next) => {
     const userEmail = req.header('Invitation')
-    console.log(userEmail)
     if (allowedEmails.includes(userEmail)) {
       next()
     } else {
